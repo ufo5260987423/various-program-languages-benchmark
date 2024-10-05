d104c78275c98bfc7dbed896ef8766d52dad54ba
Flamme Shadow <FlammeShadow@outlook.com>
2024-10-05 12:25:12 +0800

    add scheme impl: guile

---
+++ language-environments/scheme/guile/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based Chez Scheme development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    # system should match the system you are running on
+    system = "x86_64-linux";
+    #system = "x86_64-darwin";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        guile
+      ];
+      shellHook = ''
+        if [[ $OUTPUT_PATH == "[[DEFAULT]]" ]]
+          then export OUTPUT_PATH="./output/scheme-guile"
+        fi
+        echo $(uname -a) > "$OUTPUT_PATH"
+        for test in $(find ./src | grep ".scm$")
+        do
+          echo $test
+          $(which time) -av -o "$OUTPUT_PATH" guile --no-auto-compile -s $test
+        done;
+        exit
+      '';
+    };
+  };
+}
